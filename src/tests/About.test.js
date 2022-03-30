8c71062c0e6103652c32484a3a72694d57afac67
Guilherme R <grsguiga@gmail.com>
2022-03-30 20:01:18 -0300

    Requisito 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import React from 'react';
+import { About } from '../components';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const textTitle = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(textTitle).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const paragraphOne = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragraphOne).toBeInTheDocument();
+
+    const paragraphTwo = screen.getByText(/One can filter Pokémons by type/i);
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const image = screen.getByRole('img');
+
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,73 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+describe('Teste o componente <App.js />', () => {
+  test('O primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+    const firstLink = screen.getByRole('link', { name: 'Home' });
+
+    expect(firstLink).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+    const secondLink = screen.getByRole('link', { name: 'About' });
+
+    expect(secondLink).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const thirdLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(thirdLink).toBeInTheDocument();
+  });
+
+  test('Redireciona para a página inicial, na URL / ao clicar no link "Home".', () => {
+    const { history } = renderWithRouter(<App />);
+    const firstLink = screen.getByRole('link', { name: 'Home' });
+
+    userEvent.click(firstLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Redireciona para a página de /about, ao clicar no link "About".', () => {
+    const { history } = renderWithRouter(<App />);
+    const secondLink = screen.getByRole('link', { name: 'About' });
+
+    userEvent.click(secondLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Redireciona para /favorites, ao clicar no link "Favorite Pokémons".', () => {
+    const { history } = renderWithRouter(<App />);
+    const thirdLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    userEvent.click(thirdLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('Redireciona para a página "Not Found" ao entrar em uma URL desconhecida.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/notFound');
+    const notFound = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji' });
+
+    expect(notFound).toBeInTheDocument();
+  });
+});
